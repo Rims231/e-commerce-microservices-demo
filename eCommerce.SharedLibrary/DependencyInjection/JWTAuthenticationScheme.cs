aca4fd9f66d506ae49f6ce0c449d223fb9fd55e1
Rims231 <116484191+Rims231@users.noreply.github.com>
2026-04-22 09:23:28 +0100

    feat(SharedLibrary): initialise shared infrastructure library

---
+++ eCommerce.SharedLibrary/DependencyInjection/JWTAuthenticationScheme.cs
@@ -0,0 +1,47 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace eCommerce.SharedLibrary.DependencyInjection
+{
+    public static class JWTAuthenticationScheme
+    {
+        public static IServiceCollection AddJWTAuthenticationScheme(this IServiceCollection services, IConfiguration config)
+        {
+            try
+            {
+                var key = Encoding.UTF8.GetBytes(config["Authentication:Key"]!);
+                var issuer = config["Authentication:Issuer"];
+                var audience = config["Authentication:Audience"];
+
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(key),
+                            ValidateIssuer = true,
+                            ValidIssuer = issuer,
+                            ValidateAudience = true,
+                            ValidAudience = audience,
+                            ValidateLifetime = true,
+                            ClockSkew = TimeSpan.Zero
+                        };
+                    });
+            }
+            catch (Exception ex)
+            {
+                LogExceptions.LogException(ex);
+                throw;
+            }
+
+            return services;
+        }
+    }
+}
---
+++ eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateWay.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class ListenToOnlyApiGateway(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Extract specific header from the request
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+            // NULL means the request is not coming from the API Gateway
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails
+                {
+                    Status = (int)HttpStatusCode.ServiceUnavailable,
+                    Title = "Service Unavailable",
+                    Detail = "Sorry, service is unavailable. Please contact the administrator."
+                }), CancellationToken.None);
+
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}
