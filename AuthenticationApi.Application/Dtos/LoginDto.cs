ae2230331c7e91da747beea76251116a996e0242
Rims231 <116484191+Rims231@users.noreply.github.com>
2026-04-30 11:01:50 +0100

    feat(ecommerce-auth): scaffold AuthenticationApi with domain, DTOs, interfaces, and user repository

---
+++ AuthenticationApi.Application/Dtos/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationApi.Application.Dtos
+{
+    public record LoginDTO
+    (
+        [Required]
+        [EmailAddress]
+        string Email,
+
+        [Required]
+        string Password
+    );
+}
---
+++ AuthenticationApi.Application/Interfaces/IUser.cs
@@ -0,0 +1,13 @@
+﻿using AuthenticationApi.Application.Dtos;
+using AuthenticationApi.Application.DTOs;
+using eCommerce.SharedLibrary.Response;
+
+namespace AuthenticationApi.Application.Interfaces
+{
+    public interface IUser
+    {
+        Task<Response> Register(AppUserDTO appUserDTO);
+        Task<Response> Login(LoginDTO loginDTO);
+        Task<AppUserDTO> GetUser(int id);
+    }
+}
